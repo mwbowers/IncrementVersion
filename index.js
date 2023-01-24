35e93f715310770c2577936d3b2ae77ba89dacce
Matt Bowers <mwbowers81@gmail.com>
2023-01-24 11:56:04 -0600

    Change env vars.

--- index.js
+++ index.js
@@ -3,25 +3,28 @@ const os = require('os');
 
 function run()
 {
-    console.log(`VERSION: ${process.env.VERSION}`);
-    console.log(`INCREMENT_POSITION: ${process.env.INCREMENT_POSITION}`);
+    var version = process.env.VERSION || process.env.INPUT_VERSION;
+    var position = process.env.INCREMENT_POSITION || process.env.INPUT_INCREMENT_POSITION;
+
+    console.log(`VERSION: ${version}`);
+    console.log(`INCREMENT_POSITION: ${position}`);
     
     const versionRegex = new RegExp('^\d+(\.\d+){0,3}$');
 
-    if (!versionRegex.test(process.env.VERSION))
-        throw new Error(`Invalid version provided: ${process.env.VERSION}`);
+    if (!versionRegex.test(version))
+        throw new Error(`Invalid version provided: ${version}`);
     
     const positionToIncrement = 2;
 
-    if (process.env.INCREMENT_POSITION)
-        positionToIncrement = process.env.INCREMENT_POSITION - 1;
+    if (position)
+        positionToIncrement = position - 1;
 
     const versionSplit = process.env.VERSION.split('.');
 
-    if (process.env.INCREMENT_POSITION && (positionToIncrement < 0 || positionToIncrement > 3))
+    if (position && (positionToIncrement < 0 || positionToIncrement > 3))
         throw new Error('Position to increment must be between 1 and 4.');
-    if (process.env.INCREMENT_POSITION && process.env.INCREMENT_POSITION > versionSplit.length)
-        throw new Error(`Invalid increment position specified. Position specified, ${process.env.INCREMENT_POSITION}, is outside the bounds of the specified version, ${process.env.VERSION}.`);
+    if (position && position > versionSplit.length)
+        throw new Error(`Invalid increment position specified. Position specified, ${position}, is outside the bounds of the specified version, ${version}.`);
 
     versionSplit[positionToIncrement] = versionSplit[positionToIncrement] + 1;
 
@@ -30,10 +33,8 @@ function run()
     }
     
     const newVersion = versionSplit.join('.');
-    
-    console.log(`Version: ${ver}`)
-
-    process.stdout.write(`::set-output name=VERSION::${ver}` + os.EOL)
+    console.log(`Version: ${newVersion}`)
+    process.stdout.write(`::set-output name=VERSION::${newVersion}` + os.EOL)
 }
 
 run();
